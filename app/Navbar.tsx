9c09552abed33d94b52cdc73ec26c982a4ed89da
shek265 <shekharpawar265@gmail.com>
2026-01-10 23:27:31 +0530

    Active link styling added to the Navbar

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -1,7 +1,11 @@
+'use client';
 import Link from 'next/link'
 import { HiMiniBugAnt } from "react-icons/hi2";
+import { usePathname } from 'next/navigation';
+import classNames from 'classnames';
 
 const Navbar = () => {
+  const pathname = usePathname();
     const links = [
         {label:'Dashboard', href:'/'},
         {label:'Issues', href:'/issues'}
@@ -11,7 +15,12 @@ const Navbar = () => {
          <Link href='/'><HiMiniBugAnt /></Link>
          <ul className='flex space-x-6'>
             {
-                links.map(link => <Link href={link.href} key={link.href} className='text-zinc-500 hover:text-zinc-950 transition-colors'>{link.label}</Link>)
+                links.map(link => <Link href={link.href} key={link.href} className={classNames({
+                  'text-zinc-950': link.href === pathname,
+                  'text-zinc-500': link.href !== pathname,
+                  'hover:text-zinc-800 transition-colors' : true
+
+                })}>{link.label}</Link>)
             }
          </ul>
     </nav>
